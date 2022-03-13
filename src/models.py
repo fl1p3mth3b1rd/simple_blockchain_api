c215b7642d6281617e47c273375435cf97dde324
Artur <bunchathumbs@gmail.com>
2022-03-13 13:20:40 +0300

    Added pydantic response models

--- src/models.py
+++ src/models.py
@@ -4,3 +4,13 @@ from pydantic import BaseModel, HttpUrl
 class CreateRequest(BaseModel):
     media_url: HttpUrl
     owner: str
+
+class TokenResponse(BaseModel):
+    id: int
+    unique_hash: str
+    tx_hash: str
+    media_url: str
+    owner: str
+
+class TotalSupply(BaseModel):
+    total_supply: str
